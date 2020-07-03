b8f975e7ec4b0997ded836b37b016ca44a125d20
Rafael Carvalho <rafaelcarvalho.dev@gmail.com>
2020-07-03 18:42:03 -0300

    Configurando o listener para os cliques no botão e disparando um evento para o actionHandler

--- emptystate/src/main/java/br/com/rca/emptystate/ui/EmptyView.kt
+++ emptystate/src/main/java/br/com/rca/emptystate/ui/EmptyView.kt
@@ -3,13 +3,29 @@ package br.com.rca.emptystate.ui
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View.OnClickListener
 import android.widget.LinearLayout
 import br.com.rca.emptystate.R
+import kotlinx.android.synthetic.main.empty_view.view.*
 
 /**
  * Created by Rafael C. Almeida on 2020-04-07.
  */
 class EmptyView : LinearLayout {
+
+    // region Public Variables
+
+    var actionHandler: (() -> Unit)? = null
+
+    // endregion
+
+    // region Private Variables
+
+    private val actionOnClickListener = OnClickListener {
+        actionHandler?.invoke()
+    }
+
+    // endregion
 
     // region Constructors
 
@@ -36,6 +52,12 @@ class EmptyView : LinearLayout {
     private fun init() {
         val inflater = LayoutInflater.from(context)
         inflater.inflate(R.layout.empty_view, this)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        actionButton?.setOnClickListener(actionOnClickListener)
     }
 
     // endregion
