be4c646479e1fec2f6d41cce30222b61fbd64846
Rafael Carvalho <rafaelcarvalho.dev@gmail.com>
2020-07-03 19:48:16 -0300

    Implementado casos de testes para validar o comportamento do title e titleRes ao configurar diferentes entradas de dados

--- app/src/androidTest/java/br/com/rca/sample/EmptyViewEmptyStateTest.kt
+++ app/src/androidTest/java/br/com/rca/sample/EmptyViewEmptyStateTest.kt
@@ -2,14 +2,19 @@ package br.com.rca.sample
 
 import android.content.Context
 import android.content.Intent
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import br.com.rca.emptystate.model.EmptyState
 import br.com.rca.emptystate.ui.EmptyView
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 
@@ -67,6 +72,59 @@ class EmptyViewEmptyStateTest {
         emptyView = null
         context = null
         emptyState = null
+    }
+
+    @Test
+    fun testeTitleValorNull() {
+        emptyState?.title = null
+        emptyView?.test?.setupEmptyState(emptyState)
+
+        assertNull(emptyView?.title)
+        assertEquals("", emptyView?.test?.titleTextView?.text)
+        assertEquals(GONE, emptyView?.test?.titleTextView?.visibility)
+    }
+
+    @Test
+    fun testeTitleValorVazio() {
+        emptyState?.title = ""
+        emptyView?.test?.setupEmptyState(emptyState)
+
+        assertEquals("", emptyView?.title)
+        assertEquals("", emptyView?.test?.titleTextView?.text)
+        assertEquals(GONE, emptyView?.test?.titleTextView?.visibility)
+    }
+
+    @Test
+    fun testeTitleConfiguradoCorretamente() {
+        val title = "Title teste"
+        emptyState?.title = title
+        emptyView?.test?.setupEmptyState(emptyState)
+
+        assertEquals(title, emptyView?.title)
+        assertEquals(title, emptyView?.test?.titleTextView?.text)
+        assertEquals(VISIBLE, emptyView?.test?.titleTextView?.visibility)
+    }
+
+    @Test
+    fun testeTitleResValorNull() {
+        emptyState?.titleRes = null
+        emptyView?.test?.setupEmptyState(emptyState)
+
+        assertNull(emptyView?.title)
+        assertEquals("", emptyView?.test?.titleTextView?.text)
+        assertEquals(GONE, emptyView?.test?.titleTextView?.visibility)
+    }
+
+    @Test
+    fun testeTitleResConfiguradoCorretamente() {
+        val titleRes = R.string.lorem_ipsum_medium
+        emptyState?.titleRes = titleRes
+        emptyView?.test?.setupEmptyState(emptyState)
+
+        val title = context?.getString(titleRes)
+        assertEquals(title, emptyView?.title)
+        assertEquals(title, emptyView?.test?.titleTextView?.text)
+        assertEquals(VISIBLE, emptyView?.test?.titleTextView?.visibility)
     }
 
     // endregion
