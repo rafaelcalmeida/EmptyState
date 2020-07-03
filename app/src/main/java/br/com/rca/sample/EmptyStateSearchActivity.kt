858f25df859101877f86977b80e8a4a421a1116e
Rafael Carvalho <rafaelcarvalho.dev@gmail.com>
2020-07-03 19:15:53 -0300

    Implementao a EmptyStateSearchActivity e seu xml, definindo a mesma como launcher da aplicação

---
+++ app/src/main/java/br/com/rca/sample/EmptyStateSearchActivity.kt
@@ -0,0 +1,61 @@
+package br.com.rca.sample
+
+import android.os.Bundle
+import android.view.View
+import br.com.rca.emptystate.model.EmptyState
+import kotlinx.android.synthetic.main.activity_empty_state.*
+
+/**
+ * Created by Rafael C. Almeida on 14/04/20.
+ */
+class EmptyStateSearchActivity : BaseActivity() {
+
+    // region Private Variables
+
+    private val emptyState: EmptyState by lazy {
+        val emptyState = EmptyState()
+        emptyState.imageRes = R.drawable.ic_search
+        emptyState.title = "No results found."
+        emptyState.message = "we can't find any item matching your search."
+
+        emptyState
+    }
+
+    private var isLoading: Boolean = true
+        set(value) {
+            field = value
+            if (value) {
+                progressBar?.visibility = View.VISIBLE
+                emptyView?.visibility = View.GONE
+            } else {
+                progressBar?.visibility = View.GONE
+                emptyView?.visibility = View.VISIBLE
+            }
+        }
+
+    // endregion
+
+    // region Life Cycle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty_state)
+
+        setupUI()
+    }
+
+    // endregion
+
+    // region Private Methods
+
+    private fun setupUI() {
+        title = "EmptyState"
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        emptyView?.emptyState = emptyState
+        isLoading = false
+    }
+
+    // endregion
+}
